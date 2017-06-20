9b99b4faf9b72476b61cd51f98cb71052cd7ccbf
Henrique Martins <henrique.m_s@hotmail.com>
2017-06-20 10:40:28 -0300

    create new table  with  python 2.xx

---
+++ CreateTable_tb_customer_account.py
@@ -0,0 +1,22 @@
+
+import MySQLdb
+db = MySQLdb.connect(host="sql10.freemysqlhosting.net",port=3306, user='', passwd='', db="")
+
+cursor = db.cursor()
+# Delete table if already exists 
+cursor.execute("DROP TABLE IF EXISTS tb_customer_account")
+
+# Create new table
+
+sql = """CREATE TABLE tb_customer_account (
+ id_customer tinyint(6) NOT NULL AUTO_INCREMENT,
+cpf_cnpj INT(11) ,
+nm_customer varchar(30) NOT NULL,
+is_active varchar(30),
+vl_total INT(10) ,
+  PRIMARY KEY (id_customer)
+)"""
+cursor.execute(sql)
+
+# disconnect from server
+db.close()
